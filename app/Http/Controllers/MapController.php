7489dbbc77cdc9a8138a488903a30f6a668ba783
Vongola <me@vongola.tw>
2021-02-09 00:40:38 +0800

    Add basic map controller

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\MapPostRequest;
 use App\Models\Map;
-use Illuminate\Http\Request;
+use Exception;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
 
 class MapController extends Controller
@@ -11,76 +16,93 @@ class MapController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @return Application|Factory|View|Response
      */
     public function index()
     {
-        //
+        $maps = Map::all();
+        return view()->with(
+            [
+                'maps' => $maps,
+            ]
+        );
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return Response
+     * @return Application|Factory|View|Response
      */
     public function create()
     {
-        //
+        return view();
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param Request $request
-     * @return Response
+     * @param MapPostRequest $request
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(MapPostRequest $request)
     {
-        //
+        Map::create($request->only(['name']));
+        return redirect()->route('map.index');
     }
 
     /**
      * Display the specified resource.
      *
      * @param Map $map
-     * @return Response
+     * @return Application|Factory|View|Response
      */
     public function show(Map $map)
     {
-        //
+        return view()->with(
+            [
+                'map' => $map,
+            ]
+        );
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param Map $map
-     * @return Response
+     * @return Application|Factory|View|Response
      */
     public function edit(Map $map)
     {
-        //
+        return view()->with(
+            [
+                'map' => $map,
+            ]
+        );
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param Request $request
+     * @param MapPostRequest $request
      * @param Map $map
-     * @return Response
+     * @return RedirectResponse
      */
-    public function update(Request $request, Map $map)
+    public function update(MapPostRequest $request, Map $map)
     {
-        //
+        $map->update($request->only(['name']));
+        return redirect()->route('map.show', [$map]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param Map $map
-     * @return Response
+     * @return RedirectResponse
+     * @throws Exception
      */
     public function destroy(Map $map)
     {
-        //
+        $map->delete();
+        return redirect()->route('map.index');
     }
 }
