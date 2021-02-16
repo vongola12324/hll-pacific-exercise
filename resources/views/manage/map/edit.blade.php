855d74902a2ccad49c0f3a2ff33f1d4c867b917b
Vongola <me@vongola.tw>
2021-02-16 18:54:01 +0800

    Update map manage view

--- resources/views/manage/map/edit.blade.php
+++ resources/views/manage/map/edit.blade.php
@@ -5,6 +5,9 @@
 @section('console')
     <div class="flex flex-wrap">
         <h1 class="text-3xl text-white font-bold my-3">Edit Map</h1>
+        <div class="min-w-full mb-4">
+            <t-button variant="brown" href="{{ route('manage.battle.index') }}" style="width: fit-content">Back</t-button>
+        </div>
         @include('manage.map.form', ['formUrl' => route('manage.map.update', $map), 'formMethod' => 'PATCH', 'map' => $map])
     </div>
 @overwrite
