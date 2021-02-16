d2f5cbb0a04d6d2b475e3a3e7d06576aca464c1a
Vongola <me@vongola.tw>
2021-02-16 18:50:53 +0800

    Update route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\BattleController;
+use App\Http\Controllers\ForceController;
+use App\Http\Controllers\FrontpageController;
+use App\Http\Controllers\MapController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SquadController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +20,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+require __DIR__ . '/auth.php';
+
+Route::get('/', [FrontpageController::class, 'welcome'])->name('index');
+Route::get('/next', [FrontpageController::class, 'next'])->name('next');
+Route::get('/history', [FrontpageController::class, 'history'])->name('history');
+
+Route::prefix('manage')->middleware(['auth'])->as('manage.')->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('manage.battle.index');
+    })->name('dashboard');
+    Route::prefix('battle/{battle}')->group(function () {
+        Route::prefix('force/{force}')->group(function () {
+            Route::prefix('division/{division}')->group(function () {
+                Route::resource('squad', SquadController::class)->only(['index', 'show', 'destroy']);
+            });
+            Route::resource('division', ForceController::class);
+        });
+        Route::resource('force', ForceController::class);
+    });
+    Route::resource('battle', BattleController::class);
+    Route::resource('map', MapController::class)->except(['show']);
+    Route::prefix('profile')->as('profile.')->group(function () {
+        Route::get('password', [ProfileController::class, 'changePasswordView'])->name('password');
+        Route::post('password', [ProfileController::class, 'doPasswordChange'])->name('password');
+    });
+    Route::resource('user', UserController::class);
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
